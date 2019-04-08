ab495b56da791c6a07ae6b3dd65bc70dc278bcf7
syntaxp <relaxr.syntaxp@gmail.com>
2019-04-09 03:58:12 +0700

    Delete ;
    Delete ;

--- Anticapcha_/data_anti.py
+++
@@ -1,8 +0,0 @@
-Appid = "904"
-create_task_url = ("https://api.anti-captcha.com/createTask")
-get_result_url = ("https://api.anti-captcha.com/getTaskResult")
-get_balance_url = ("https://api.anti-captcha.com/getBalance")
-incorrect_captcha_url = ("https://api.anti-captcha.com/reportIncorrectImageCaptcha")
-get_queue_status_url = ("https://api.anti-captcha.com/getQueueStats")
-header = { "Accept": "application/json","Content-Type": "application/json"}
-cloneid = ("https://raw.githubusercontent.com/syntaxp/PserverNA/master/Anticapcha_/data_anti.py")
